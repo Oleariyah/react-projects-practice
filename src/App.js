392690e9d3352964530265f23f2becf44e158155
Sodiq Olaiya <sodiq.olaiya@centricgateway.com>
2020-09-15 11:51:41 +0100

    created a react-project-setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
-import { Home, Features, Login, Pricing, Resources, Support } from "./pages";
+import { Home } from "./pages";
 import "./styles/App.css";
 
 export default () => {
@@ -8,12 +8,6 @@ export default () => {
     <Fragment>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/features" component={Features} />
-        <Route path="/login" component={Login} />
-        <Route path="/pricing" component={Pricing} />
-        <Route path="/features" component={Features} />
-        <Route path="/resources" component={Resources} />
-        <Route path="/support" component={Support} />
       </Switch>
     </Fragment>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,3 @@
-import Features from "./Features";
 import Home from "./Home";
-import Pricing from "./Pricing";
-import Support from "./Support";
-import Resources from "./Resources";
-import Login from "./Login";
 
-export { Features, Home, Login, Support, Resources, Pricing };
+export { Home };
